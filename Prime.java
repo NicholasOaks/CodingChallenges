9079f34e5a21674a0c4182c84e0382430fa18220
NicholasOchs <70531634+NicholasOchs@users.noreply.github.com>
2022-08-03 13:03:11 -0400

    testing

---
+++ Prime.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Prime{
+    public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+
+     System.out.println("Please enter a number");
+     int x = scanner.nextInt();
+
+        if(x==1){
+            System.out.println("This is not a prime number");
+        }
+
+        for(int y = x-1;y > 0;y--){
+
+            if(y == 1){
+                System.out.println("This is a prime number");
+                break;
+            }
+            if(x%y==0){
+                System.out.println("This is not prime number");
+                break;
+            }
+        }
+    }
+}
